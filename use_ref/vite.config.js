83bc2bfc55d0c90351c01de8902493817cf0e04c
Hein Thit Swan <heinthitswan@gmail.com>
2025-12-26 16:00:38 +0700

    fix

--- use_ref/vite.config.js
+++ use_ref/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/useref_deploy-/'
+  base: '/use_ref_deploy/'
 })
